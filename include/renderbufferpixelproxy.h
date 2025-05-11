ca519409bc7c0268a08800b5d8f18c961bca1191
chevechette <karina.martynava@gmail.com>
2025-05-11 16:22:05 +0200

    recompile update

---
+++ include/renderbufferpixelproxy.h
@@ -0,0 +1,17 @@
+#pragma once 
+
+#include "structs.h"
+
+class RenderBufferPixelProxy { // assuming format GL_RGBA
+    private :
+        unsigned char   &r;
+        unsigned char   &g;
+        unsigned char   &b;
+        unsigned char   &a;
+    public :
+        RenderBufferPixelProxy(unsigned char &r, unsigned char &g, unsigned char &b, unsigned char &a);
+        ~RenderBufferPixelProxy();
+
+        RenderBufferPixelProxy  &operator=(const RenderBufferPixelProxy &pix);
+        RenderBufferPixelProxy  &operator=(const Color &col);
+};
--- src/guimanager.cpp
+++ src/guimanager.cpp
@@ -107,37 +107,7 @@ class ObjectManager {
 
 // Is basically the image in the background for the GUI
 
-class RenderBufferPixelProxy { // assuming format GL_RGBA
-    private :
-        unsigned char   &r;
-        unsigned char   &g;
-        unsigned char   &b;
-        unsigned char   &a;
-    public :
-        RenderBufferPixelProxy(unsigned char &r, unsigned char &g, unsigned char &b, unsigned char &a) :
-        r(r), g(g), b(b), a(a) {
-        }
-        ~RenderBufferPixelProxy() {}
-
-        RenderBufferPixelProxy  &operator=(const RenderBufferPixelProxy &pix) {
-            if (this == &pix)
-                return *this;
-            r = pix.r;
-            g = pix.g;
-            b = pix.b;
-            a = pix.a;
-            return *this;
-        }
-
-        RenderBufferPixelProxy  &operator=(const Color &col) {
-            r = col.r * 0xFF;
-            g = col.g * 0xFF;
-            b = col.b * 0xFF;
-            a = col.a * 0xFF;
-            return *this;
-        }
-};
-
+#include "renderbufferpixelproxy.h"
 
 class RenderBufferRowProxy { // assuming format GL_RGBA
     private :
