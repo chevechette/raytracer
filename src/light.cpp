68737374e0a3ee212cae8aa5aedf24f084258ca9
chevechette <karina.martynava@gmail.com>
2025-06-28 01:18:00 +0200

    choosing next topic

---
+++ src/light.cpp
@@ -0,0 +1,18 @@
+#include "structs.h"
+enum LightType {Sun, Spot};
+
+class Light {
+    private:
+    Color   col;
+    float   lux; // Lux power, could be an int or a 0-1 range.
+    Coordinates pos; // If spot importnat, if sun, then this is a dir ? Complicated
+    
+    protected:
+
+    public:
+        Light();
+        ~Light();
+
+
+
+};
